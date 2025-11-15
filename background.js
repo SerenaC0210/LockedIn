e0eeec56eb8dbd7d6211b6d6c7ceae923f3ea96a
Lucy <yangl2021@gmail.com>
2025-11-15 16:45:30 -0500

    code now jumps to first item in whitelist if you open a non-whitelisted tab
    updates to whitelist and redirect mechanics

--- background.js
+++ background.js
@@ -2,5 +2,32 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "openNewTab") {
     chrome.tabs.create({ url: message.url });
+  } else if (message.action === "switchToWhitelistedTab") {
+    findTabByUrl(message.whitelist);
   }
 });
+
+function findTabByUrl(matches) {
+
+  chrome.tabs.query({}, function(tabs) {
+    let foundTab = null;
+    for (let i = 0; i < tabs.length; i++) {
+      if (tabs[i].url) {
+        for (let j = 0; j < matches.length; j++) {
+          if (tabs[i].url.includes(matches[j])) {
+            foundTab = tabs[i];
+            break;
+          }
+        }
+      }
+    }
+
+    if (foundTab) {
+      console.log("Tab with URL found: ", foundTab);
+      chrome.tabs.update(foundTab.id, { active: true });
+    } else {
+      console.log("Tab with URL not found.");
+      chrome.tabs.create({ url: matches[0] });
+    }
+  });
+}
